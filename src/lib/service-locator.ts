3e59f0e99506c094df542c2c55447d4d173ca917
smulas <simone.mulas@gmail.com>
2016-12-29 23:55:27 +0100

    sm

--- src/lib/service-locator.ts
+++ src/lib/service-locator.ts
@@ -62,7 +62,7 @@ export function getService(name: string) {
 }
 
 export const call = (serviceName: string) => (method: string) => (args: any, headers?: any) => {
-    logger (`call ${serviceName}/${method}`);
+    logger (`call ${serviceName}/${method} ${JSON.stringify(args)}`);
     return getService(serviceName).then(endpoint => {
         return fetch(endpoint + '/' + method, {
             headers,
