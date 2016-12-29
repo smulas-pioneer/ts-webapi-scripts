6cca3f3f3b3bc5b65b05e01e8fc547daaba1bfbd
simo <simo@SM-MACPRO.local>
2016-12-29 16:22:02 +0100

    sm

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,2 @@
-import {test} from './test';
-import * as  program from 'commander';
 export * from './express';
 
-program
-    .version('1.0.0')
-    .option('-t, --test <dir>','Run tests on selected dir')
-    .parse(process.argv);
-
-if ( program['test']){
-    test(program['test']);
-}
