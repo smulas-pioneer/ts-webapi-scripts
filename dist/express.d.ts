c268aab3dd48904f98646e7e4f2d0c5b7cbf63b9
simo <simo@SM-MACPRO.local>
2016-12-29 16:17:28 +0100

    sm

--- dist/express.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function registerPost<TArg, TRes>(method: <TArg, TRes>(arg: TArg) => Promise<TRes>): void;
-export declare function start(port: number): void;
--- dist/test.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function test(dir: string): void;
